6dbdabc5b8e0daea11488641457379925b716a74
Maria <you@example.com>
2024-10-04 22:59:43 +0300

    add the script, analyse statistics

--- my_pass/README.md
+++ my_pass/README.md
@@ -1,2 +1,8 @@
-clang++ ../my_pass/MyPass.cpp -fPIC -shared -I`llvm-config --includedir` -o libPass.so
-clang -Xclang -load -Xclang ./libPass.so app.c -emit-llvm -S -O2 app.ll > ../my_pass/info.txt
+### How to test files in this module
+
+```clang++ ../my_pass/MyPass.cpp -fPIC -shared -I`llvm-config --includedir` -o libPass.so```
+
+```clang -Xclang -load -Xclang ./libPass.so app.c -emit-llvm -S -O2 app.ll > ../my_pass/info.txt```
+
+To run my script (cout statistics): 
+```python3 pattern_analyzer.py```
---
+++ my_pass/pattern_analyzer.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from collections import Counter
+
+def main():
+  """Анализирует пары инструкций в файле, 
+     строит график и сохраняет статистику."""
+
+  opcode_pairs = Counter()
+  total_pairs = 0
+
+  with open("info.txt", "r") as file:
+    for line in file:
+      names = [name.strip() for name in line.strip().split("->") if "phi" not in name]
+      for i in range(len(names) - 1):
+        opcode_pairs[f"{names[i]} -> {names[i+1]}"] += 1
+        total_pairs += 1
+        
+  with open("opcode_statistics.txt", "w") as f:
+    for pair, count in opcode_pairs.items():
+      f.write(f"{pair}: {(count / total_pairs) * 100:.2f}%\n")
+  plt.clf()
+  colors = plt.cm.viridis([i / len(opcode_pairs) for i in range(len(opcode_pairs))])
+  
+  plt.figure(figsize=(10, 5))
+  plt.bar(opcode_pairs.keys(), [(count / total_pairs) * 100 for count in opcode_pairs.values()], color=colors)
+  plt.xlabel("Пары инструкций")
+  plt.ylabel("Процентное соотношение (%)")
+  plt.title("Статистика пар инструкций")
+  plt.xticks(rotation=45, ha="right")
+  plt.tight_layout()
+  plt.savefig("opcode_statistics.png")
+
+if __name__ == "__main__":
+  main()
